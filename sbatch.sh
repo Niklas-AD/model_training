60e263a4f0ba24f204345efc165bec9812a9ee7f
Niklas-AD <97914640+Niklas-AD@users.noreply.github.com>
2022-06-08 18:01:42 +0200

    Update sbatch.sh

--- sbatch.sh
+++ sbatch.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
-#SBATCH -p gpu_8
-#SBATCH --gres=gpu:8
+#SBATCH -p gpu_4
+#SBATCH --gres=gpu:1
 #SBATCH --nodes=1
-#SBATCH --job-name=gpu_test
+#SBATCH --job-name=swin_training
 #SBATCH --time=2:30:00
 # Further options: https://wiki.bwhpc.de/e/BwUniCluster_2.0_Slurm_common_Features
 
 
 #Make output directory
-FOO=run_$(date +%Y-%m-%d_%H-%M-%S)
+FOO=swinl_run_$(date +%Y-%m-%d_%H-%M-%S)
 mkdir $FOO
 
 #Datensatz nach $TMP Kopieren
-cp -r ReducedDataset $TMP
+cp -r cityscapes $TMP
 
-./training.run -w -m $TMP/ReducedDataset/:Mask2Former/datasets -m $FOO:Mask2Former/output python3 train_net.py --config-file configs/cityscapes/panoptic-segmentation/swin/maskformer2_swin_large_IN21k_384_bs16_90k.yaml --num-machines 1 --num-gpus 8
+#No speedup by more GPUs - Reason Unclear, doesn´t seem to be CPU bottleneck - maybe memory
+#./training.run -w -m $TMP/cityscapes:Mask2Former/datasets -m $FOO:Mask2Former/output python3 train_net.py --config-file configs/cityscapes/panoptic-segmentation/swin/maskformer2_swin_large_IN21k_384_bs16_90k.yaml --num-machines 1 --num-gpus 8
 
+./training.run -w -m $TMP:Mask2Former/datasets -m $FOO:Mask2Former/output python3 train_net.py --config-file configs/cityscapes/panoptic-segmentation/swin/maskformer2_swin_large_IN21k_384_bs16_90k.yaml --num-machines 1 --num-gpus 1 SOLVER.IMS_PER_BATCH 2
 
